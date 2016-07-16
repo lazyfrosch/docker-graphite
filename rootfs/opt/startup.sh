7ab7ea2b06b85edae6aefb0e5f3abea0c39f5882
Bodo Schulz <support@xanhaem.de>
2016-07-16 15:45:38 +0200

    styles

--- rootfs/opt/startup.sh
+++ rootfs/opt/startup.sh
@@ -84,14 +84,14 @@ configureDatabase() {
       chmod 0664 ${WORK_DIR}/graphite/storage/graphite.db
     fi
 
-      sed -i \
-        -e "s|%DBA_FILE%|${WORK_DIR}/graphite/storage/graphite.db|" \
-        -e 's|%DBA_ENGINE%|sqlite3|g' \
-        -e "s|%DBA_USER%||g" \
-        -e "s|%DBA_PASS%||g" \
-        -e "s|%DBA_HOST%||g" \
-        -e "s|%DBA_PORT%||g" \
-        ${CONFIG_FILE}
+    sed -i \
+      -e "s|%DBA_FILE%|${WORK_DIR}/graphite/storage/graphite.db|" \
+      -e 's|%DBA_ENGINE%|sqlite3|g' \
+      -e "s|%DBA_USER%||g" \
+      -e "s|%DBA_PASS%||g" \
+      -e "s|%DBA_HOST%||g" \
+      -e "s|%DBA_PORT%||g" \
+      ${CONFIG_FILE}
 
   elif [ "${DATABASE_TYPE}" == "mysql" ]
   then
